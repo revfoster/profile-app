e4003202d86bd9ca94c3d57bb80c7b9c64687227
foster <fosterogbodo20@gmail.com>
2026-03-08 01:03:09 +0000

    I updated pricing.jsx

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -20,7 +20,7 @@ export default function Pricing() {
       <div className="container-sm">
         <div className="pricing-inner section-inner">
           <div className="pricing-header text-center">
-            <h2 className="section-title mt-0">Unlimited for all</h2>
+            <h2 className="section-title mt-0">My Cloud Vision</h2>
             <p className="section-paragraph mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut ad quis nostrud.
             </p>
