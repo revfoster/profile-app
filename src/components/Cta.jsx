e6e6e7543e47e2523f939d29e44f974d73bde118
foster <fosterogbodo20@gmail.com>
2026-03-08 01:23:24 +0000

    I updated the Cta.jsx

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -5,7 +5,7 @@ export default function Cta() {
     <section className="cta section">
       <div className="container">
         <div className="cta-inner section-inner">
-          <h3 className="section-title mt-0">Still not convinced on buying?</h3>
+          <h3 className="section-title mt-0">Hire me right now?</h3>
           <div className="cta-cta">
             <a className="button button-primary button-wide-mobile" href="#">Get in touch</a>
           </div>
